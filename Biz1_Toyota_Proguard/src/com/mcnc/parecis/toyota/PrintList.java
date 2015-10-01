0b1364205927aca29e3e205263ae48e91a4bb173
jslee <jslee@JSLEE-IT.tfskr.co.kr>
2015-10-01 16:24:13 +0900

    dfgdsgdsfgsdfgdsfgdsfgdfsg

--- Biz1_Toyota_Proguard/src/com/mcnc/parecis/toyota/PrintList.java
+++ Biz1_Toyota_Proguard/src/com/mcnc/parecis/toyota/PrintList.java
@@ -2,7 +2,6 @@ package com.mcnc.parecis.toyota;
 
 import com.mcnc.parecis.bizmob.view.ImpMainActivity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
